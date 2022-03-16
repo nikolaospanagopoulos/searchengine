a10468f4b6699ce2072330a3288b9e2f29b191fb
nikolaos panagopoulos <nikos4222@outlook.com.gr>
2022-03-16 16:52:17 +0200

    prod db

--- App/Config.php
+++ App/Config.php
@@ -9,14 +9,15 @@ namespace App;
 class Config{
      
 
-    const DB_HOST = 'localhost';
+    const DB_HOST = 'mariadb-72354-0.cloudclusters.net';
 
     const DB_NAME = 'google';
 
-    const DB_USER = 'root';
+    const DB_USER = 'nikos';
 
-    const DB_PASSWORD = '';
+    const DB_PASSWORD = '6942640822';
 
+    const DB_PORT = '16542';
 
     const SHOW_ERRORS = true;
 }
--- Core/Model.php
+++ Core/Model.php
@@ -17,7 +17,7 @@ abstract class Model
         if ($db === null) {
 
 
-            $dsn = 'mysql:host=' . Config::DB_HOST . ';dbname=' . Config::DB_NAME . ';charset=utf8';
+            $dsn = 'mysql:host=' . Config::DB_HOST . ';dbname=' . Config::DB_NAME . ';charset=utf8;port='.Config::DB_PORT;
             $db = new PDO($dsn, Config::DB_USER, Config::DB_PASSWORD);
 
              
